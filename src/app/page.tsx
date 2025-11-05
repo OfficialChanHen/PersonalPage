3b2cd9492fc73ac22749bc67faa60bc9a56c57df
Chan Hen <officialchanhen@gmail.com>
2025-11-04 22:39:55 -0600

    Rotating card implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <div className="min-w-screen min-h-screen flex flex-col justify-center items-center bg-pictonblue">
+        <div className="group w-125 h-75 perspective-[1000px]">
+          <div className="relative w-full h-full text-center transition-transform duration-800 transform-3d group-hover:rotate-y-180">
+            <div className="absolute flex flex-col justify-center items-center w-full h-full [-webkit-backface-visibility:hidden] bg-white text-black shadow-lg/20">
+              <h1>Welcome to my Portfolio</h1>
+            </div>
+            <div className="absolute flex flex-col justify-center items-center w-full h-full [-webkit-backface-visibility:hidden] rotate-y-180 bg-white text-black shadow-lg/20">
+              <h1>Chan Hen</h1>
+              <p>Software Engineer</p>
+              <p>Other Info</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
