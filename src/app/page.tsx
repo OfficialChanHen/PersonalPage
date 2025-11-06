2ea4a11ef674e7249687056466f18d3dfef8a7a7
Chan Hen <officialchanhen@gmail.com>
2025-11-05 19:43:21 -0600

    Basic data for card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,36 @@
+"use client"
+
+import { useState } from "react";
+
 export default function Home() {
+  const [isBackside, setIsBackside] = useState(false);
+
+  const handleRotation = () => {
+    setIsBackside(prev => !prev);
+  }
+
   return (
     <main>
-      <div className="min-w-screen min-h-screen flex flex-col justify-center items-center bg-pictonblue">
-        <div className="group w-125 h-75 perspective-[1000px]">
-          <div className="relative w-full h-full text-center transition-transform duration-800 transform-3d group-hover:rotate-y-180">
-            <div className="absolute flex flex-col justify-center items-center w-full h-full [-webkit-backface-visibility:hidden] bg-white text-black shadow-lg/20">
-              <h1>Welcome to my Portfolio</h1>
+      <div className="relative min-w-screen min-h-screen flex flex-col justify-start items-center p-10 bg-greenblue">
+        <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-125 h-75 perspective-[1000px]">
+          <div 
+            className={`relative size-full text-center transition-transform duration-800 transform-3d ${isBackside && "rotate-y-180"}`}
+            onClick={handleRotation}
+          >
+            <div className="absolute flex flex-col justify-center items-center size-full p-3 [-webkit-backface-visibility:hidden] bg-pictonblue rounded-lg text-white drop-shadow-2xl/40">
+              <div className="flex flex-col justify-center items-center size-full py-2 border-3 rounded-lg border-parchment text-parchment">
+                  {/*<span className="text-pictonblue">Hello!</span>*/}
+                  <div className="flex-1 flex justify-center items-center">
+                    <span className="text-4xl text-center">Welcome to my <br/>Portfolio</span>
+                  </div>
+                  <span>Tap the card for more info!</span>
+              </div>
             </div>
-            <div className="absolute flex flex-col justify-center items-center w-full h-full [-webkit-backface-visibility:hidden] rotate-y-180 bg-white text-black shadow-lg/20">
-              <h1>Chan Hen</h1>
-              <p>Software Engineer</p>
-              <p>Other Info</p>
+            <div className="absolute flex flex-col justify-start items-baseline size-full p-10 [-webkit-backface-visibility:hidden] rotate-y-180 bg-parchment rounded-lg text-black drop-shadow-2xl/40 text-left tracking-[0.3rem] text-pictonblue">
+              <span className="text-3xl text-greenblue">Chan Hen</span>
+              <span className="text-lg">Software Engineer</span>
+              <span className="text-md my-auto">(952)-994-5059<br/>Officialchanhen@gmail.com</span>
+              <span className="mt-auto">Other Info</span>
             </div>
           </div>
         </div>
